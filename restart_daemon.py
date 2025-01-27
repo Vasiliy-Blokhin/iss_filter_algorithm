caeaff1868c0e1bc5b33fbcb04af6e7afb65c743
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-01-27 12:41:30 +1000

    correct restart time

--- restart_daemon.py
+++ restart_daemon.py
@@ -23,7 +23,7 @@ while True:
         diff_time = 2 * TIME_UPDATE / 60
         activity_time = a.get_all_data(table=tables.Activity)[0]['last_time']
         current_time = a.curent_msc_time()
-        if activity_time < current_time - diff_time:
+        if activity_time < current_time - diff_time or current_time == 0.0:
             subprocess.run(['sudo', 'systemctl', 'restart', SERVICE_NAME])
             tlg.send_message(text=RESTART_MESSAGE)
             logger.info(RESTART_MESSAGE)
